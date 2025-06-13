adb40dd7c33d6002c50f6185e2e38a68b97aa43e
soheeGit <63wlsthgml@gmail.com>
2025-06-13 09:33:23 +0900

    초기설정

---
+++ src/main/java/org/example/titanthoughts/TitanThoughtsApplication.java
@@ -0,0 +1,13 @@
+package org.example.titanthoughts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanThoughtsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TitanThoughtsApplication.class, args);
+    }
+
+}
